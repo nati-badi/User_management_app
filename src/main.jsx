34013546f5685769c65c91bb4dd60247306c7f94
BaDi <natibadigojimma@gmail.com>
2024-02-02 03:38:10 -0800

    Configure the main.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,19 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
+import { Provider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
+
+export const store = configureStore({
+  reducer: {
+    // name: 
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Provider store={store}>
       <App />
-  </React.StrictMode>,
+    </Provider>,
+  </React.StrictMode>
 )
