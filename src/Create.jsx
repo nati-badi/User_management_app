87b161046b1d19faab469145ec100a616aa8e9a9
BaDi <natibadigojimma@gmail.com>
2024-02-02 06:01:35 -0800

    Build a form for the create component

--- src/Create.jsx
+++ src/Create.jsx
@@ -1,8 +1,22 @@
-
 function Create() {
   return (
-    <div>Create</div>
-  )
+    <div className="d-flex w-100 vh-100 justify-content-center align-items-center">
+      <div className="w-50 border bg-secondary text-white p-5">
+        <h3>Add New User</h3>
+        <form>
+          <div>
+            <label htmlFor="name">Name:</label>
+            <input type="text" name="name" className="form-control" placeholder="Eg. Natnael Badigo" autoComplete="hidden"/>
+          </div>
+          <div>
+            <label htmlFor="email">Email:</label>
+            <input type="email" name="email" className="form-control" placeholder="Eg. natibadigo@gmail.com" autoComplete="hidden"/>
+          </div><br />
+          <button className="btn btn-info">Submit</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default Create
+export default Create;
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 function Home() {
   const users = useSelector((state) => state.users);
@@ -6,7 +7,7 @@ function Home() {
   return (
     <div className="container">
       <h2>Crud App with JSON Server</h2>
-      <button className="btn btn-success my-3">Create +</button>
+      <Link to='/create' className="btn btn-success my-3">Create +</Link>
       <table className="table">
         <thead>
           <tr>
